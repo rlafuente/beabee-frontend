04bfe8b55b15e09d4d68589212cbb1375391a896
porivo <poriya.ap@pm.me>
2021-12-01 19:24:36 +0300

    feat: add CompactStatus component

---
+++ src/components/CompactStatus.vue
@@ -0,0 +1,37 @@
+<template>
+  <!-- TODO: improve accessibilty -->
+  <span class="w-2 h-2 inline-block rounded-full" :class="bgClass" />
+</template>
+
+<script lang="ts" setup>
+import { computed } from 'vue';
+
+const props = defineProps({
+  status: {
+    type: String,
+    default: 'active',
+    validator(value) {
+      return ['active', 'expired', 'primary', 'empty'].includes(value);
+    },
+  },
+});
+
+const bgClass = computed(() => {
+  switch (props.status) {
+    case 'primary':
+      return 'bg-primary';
+      break;
+
+    case 'expired':
+      return 'bg-danger';
+      break;
+
+    case 'empty':
+      return 'border border-primary-80';
+      break;
+
+    default:
+      return 'bg-success';
+  }
+});
+</script>
